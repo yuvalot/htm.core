692aec995a949b85fd06286114102061a9fa5c91
Marek Otahal <markotahal@gmail.com>
2019-10-02 12:28:09 +0200

    Eye: rm arg fovea_scale
    keep it only as member variable

--- py/htm/encoders/eye.py
+++ py/htm/encoders/eye.py
@@ -133,12 +133,10 @@ class Eye:
     """
     def __init__(self,
         output_diameter   = 200,
-        fovea_scale       = .177, #TODO rm as arg?
         sparsity          = .2,): #TODO add arg mode: parvo, magno, both
         """
         Argument output_diameter is size of output ... output is a 
             field of view (image) with circular shape
-        Argument fovea_scale  ... represents "zoom" aka distance from the object/image. 
         Argument sparsity is fraction of bits in eye.output_sdr which are 
             active, on average.
         """
@@ -148,7 +146,8 @@ class Eye:
         # is reduced by this factor back to the output_diameter.
         self.resolution_factor = 3
         self.retina_diameter   = int(self.resolution_factor * output_diameter)
-        self.fovea_scale       = fovea_scale
+        # Argument fovea_scale  ... represents "zoom" aka distance from the object/image.
+        self.fovea_scale       = 0.177
         assert(output_diameter // 2 * 2 == output_diameter) # Diameter must be an even number.
         assert(self.retina_diameter // 2 * 2 == self.retina_diameter) # (Resolution Factor X Diameter) must be an even number.
 
