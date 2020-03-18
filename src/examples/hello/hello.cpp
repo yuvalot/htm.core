754a0c2d27cd52301a676e9904662939e45bcc5e
David Keeney <dkeeney@gmail.com>
2020-03-18 10:31:39 -0700

    change case on example hello.cpp

--- src/examples/hello/hello.cpp
+++ src/examples/hello/hello.cpp
@@ -2,7 +2,7 @@
 
 #include <string> // stoi
 
-//this runs as executable
+//this runs as executable:  hello [epochs]
 int main(int argc, char* argv[]) {
   htm::UInt EPOCHS = 5000; // number of iterations (calls to SP/TP compute() )
 
