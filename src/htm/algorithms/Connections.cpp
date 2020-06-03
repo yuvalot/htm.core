b58258cb400abd7449b3b53ba070755d48662f39
Marek Otahal <markotahal@gmail.com>
2020-06-03 14:23:16 +0200

    COnn revert to lower_bound with explicit ordering
    for platform-independent build?

--- src/htm/algorithms/Connections.cpp
+++ src/htm/algorithms/Connections.cpp
@@ -274,9 +274,11 @@ void Connections::destroySynapse(const Synapse synapse) {
     }
   }
   
-  const auto synapseOnSegment = std::find(segmentData.synapses.cbegin(), 
+  const auto synapseOnSegment = std::lower_bound(segmentData.synapses.cbegin(), 
 		                          segmentData.synapses.cend(),
-					  synapse); 
+					  synapse,
+					  [&](const Synapse a, const Synapse b) -> bool { return dataForSynapse(a).id < dataForSynapse(b).id;}
+					  ); 
 
   NTA_ASSERT(synapseOnSegment != segmentData.synapses.cend());
   NTA_ASSERT(*synapseOnSegment == synapse);
