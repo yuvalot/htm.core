fd8d3351e650f964328b86091ed011a4607800c4
Zbysek Zapadlik <zbysekzapadlik@seznam.cz>
2020-07-08 02:48:09 -0400

    Doc details

--- src/examples/napi_hello/napi_hello.cpp
+++ src/examples/napi_hello/napi_hello.cpp
@@ -99,7 +99,7 @@ int main(int argc, char* argv[]) {
     //                                 |
     //                         .-----------------.
     //                         |      tm         |
-    //                         |   (TMRegion)    |
+    //                         |   (TMRegion)    |---->CSV file
     //                         |                 |
     //                         `-----------------'
     //
