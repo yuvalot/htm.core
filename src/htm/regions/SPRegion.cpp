a2b8ab13f48eab71aa06326d86d2af46e51bd45a
Zbysek Zapadlik <zbysekzapadlik@seznam.cz>
2020-09-02 14:52:11 +0200

    Apostrophe instead of quotation marks

--- src/htm/regions/SPRegion.cpp
+++ src/htm/regions/SPRegion.cpp
@@ -372,7 +372,7 @@ Spec *SPRegion::createSpec() {
       ParameterSpec("(float)\n"
                     "The default connected threshold.Any synapse whose "
                     "permanence value is "
-                    "above the connected threshold is a \"connected synapse\", "
+                    "above the connected threshold is a 'connected synapse', "
                     "meaning it can "
                     "contribute to the cell's firing. Default ``0.1``.",
                     NTA_BasicType_Real32,             // type
@@ -404,7 +404,7 @@ Spec *SPRegion::createSpec() {
           "when "
           "either its previously learned inputs are no longer ever active, or "
           "when "
-          "the vast majority of them have been \"hijacked\" by other "
+          "the vast majority of them have been 'hijacked' by other "
           "columns.Default "
           "``0.001``.",
           NTA_BasicType_Real32,             // type
